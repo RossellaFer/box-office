186acb5871fdb10b7f674aa3d4b7cdfdb24b19a0
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2025-01-11 17:02:48 +0100

    favicon

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -19,7 +19,6 @@ const TitleWrapper = styled.div`
   margin: 0 0 40px;
   h1 {
     color: ${({ theme }) => theme.mainColors.blue};
-    letter-spacing: 10px;
     text-transform: uppercase;
     margin: 0 0 10px;
   }
